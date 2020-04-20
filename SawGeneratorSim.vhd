a36bbc2744e4119c3919dd97eff2dccd288991c5
SmutnyPan <wojtek.sadlik38@gmail.com>
2020-04-20 16:07:07 +0200

    komentarze - nuta przy klawiszu

--- SawGeneratorSim.vhd
+++ SawGeneratorSim.vhd
@@ -92,7 +92,7 @@ BEGIN
 	STIM_Proc: process 					--proces do testowania
 	BEGIN
 	
-		DI <= X"1C"; 							--wciœniêcie klawisza 1C -> A na klawiaturze
+		DI <= X"1C"; 							--wciœniêcie klawisza 1C -> A na klawiaturze (C' 261.6256Hz)
 		DI_Rdy <= '1', '0' after 20 ns;		
 		wait for 4 ms;							--trzymanie klawisza przez 4 ms
 		F0 <= '1', '0' after 20 ns;		--puszczenie klawisza
@@ -100,7 +100,7 @@ BEGIN
 		
 		wait for 20 ns;
 		
-		DI <= X"42"; 							--wciœniêcie klawisza 42 -> K na klawiaturze
+		DI <= X"42"; 							--wciœniêcie klawisza 42 -> K na klawiaturze (C'' 523.2511Hz)
 		DI_Rdy <= '1', '0' after 20 ns;		
 		wait for 2 ms;							--trzymanie klawisza przez 2 ms
 		F0 <= '1', '0' after 20 ns;		--puszczenie klawisza
